8edfc415964bea119835280c0fe2f9f1d69ea556
rsurgiewicz <rafal@surgiewicz.pl>
2021-11-10 16:16:59 +0100

    adding option for host port

--- jprq/main.py
+++ jprq/main.py
@@ -18,9 +18,11 @@ def main():
 @click.argument('port')
 @click.option('-s', '--subdomain', default='')
 @click.option('--host', default='open.jprq.io')
+@click.option('--hostport', default='80')
 def http(**kwargs):
     host = kwargs['host']
     port = kwargs['port']
+    hostport = kwargs['hostport']
     username = kwargs['subdomain'] or getuser()
 
     print(f"\n\033[1;35mjprq : {__version__}\033[00m \033[34m{'Press Ctrl+C to quit.':>60}\n")
@@ -29,7 +31,7 @@ def http(**kwargs):
     try:
         loop.run_until_complete(
             open_http_tunnel(
-                ws_uri=f'wss://{host}/_ws/?username={username}&port={port}&version={__version__}',
+                ws_uri=f'wss://{host}:{hostport}/_ws/?username={username}&port={port}&version={__version__}',
                 http_uri=f'http://127.0.0.1:{port}',
             )
         )
@@ -40,8 +42,10 @@ def http(**kwargs):
 @main.command()
 @click.argument('port', type=click.INT)
 @click.option('--host', default='tcp.jprq.io')
+@click.option('--hostport', default='80')
 def tcp(**kwargs):
     host = kwargs['host']
+    hostport = kwargs['hostport']
     port = kwargs['port']
 
     print(f"\n\033[1;35mjprq : {__version__}\033[00m \033[34m{'Press Ctrl+C to quit.':>60}\n")
@@ -49,7 +53,7 @@ def tcp(**kwargs):
     loop = asyncio.get_event_loop()
     try:
         loop.run_until_complete(
-            open_tcp_tunnel(remote_server_host=host, ws_uri=f'wss://{host}/_ws/', local_server_port=port)
+            open_tcp_tunnel(remote_server_host=host, ws_uri=f'wss://{host}:{hostport}/_ws/', local_server_port=port)
         )
     except KeyboardInterrupt:
         print("\n\033[31mjprq tunnel closed\033[00m")
